2cadcc18a202f50bde2fad435914d8882dd2c00d
Doctor <75563269+seniordoctor@users.noreply.github.com>
2023-02-10 20:57:49 +0300

    Master 3

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
 
 const app = express();
+
+// TEMPLATE ENGINE
+app.set('view engine', 'ejs');
 
 // const myLogger = (req, res, next) => {
 //   console.log('Middleware Log 1');
@@ -14,15 +18,21 @@ const app = express();
 // };
 
 // MIDDLEWARES
-
 app.use(express.static('public'));
 
 // app.use(myLogger);
 // app.use(myLogger2);
 
+// ROUTES
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html')); 
+  res.render('index');
 });
+app.get('/about', (req, res) => {
+    res.render('about');
+  });
+  app.get('/add', (req, res) => {
+    res.render('add');
+  });
 
 const port = 3000;
 app.listen(port, () => {
